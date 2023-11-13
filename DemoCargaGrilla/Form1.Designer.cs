35436346de17b779f159f2d1e886d570c60ffe40
Mauro Favre <mauro_favre@hotmail.com>
2023-11-12 23:05:40 -0300

    Agregar archivos de proyecto.

---
+++ DemoCargaGrilla/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace DemoCargaTextBoxMultilinea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.chkPrecio = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(86, 33);
+            this.txtCodigo.MaxLength = 6;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(75, 20);
+            this.txtCodigo.TabIndex = 0;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            this.txtCodigo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCodigo_KeyDown);
+            this.txtCodigo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCodigo_KeyPress);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(86, 69);
+            this.txtNombre.MaxLength = 30;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(215, 20);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(319, 22);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(93, 40);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "&Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Datos cargados";
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.AllowUserToAddRows = false;
+            this.dgvDatos.AllowUserToDeleteRows = false;
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvDatos.Location = new System.Drawing.Point(39, 201);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.ReadOnly = true;
+            this.dgvDatos.RowHeadersVisible = false;
+            this.dgvDatos.Size = new System.Drawing.Size(373, 150);
+            this.dgvDatos.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Código";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Precio";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(86, 106);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(75, 20);
+            this.txtPrecio.TabIndex = 2;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(319, 131);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(93, 41);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "&Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // chkPrecio
+            // 
+            this.chkPrecio.AutoSize = true;
+            this.chkPrecio.Location = new System.Drawing.Point(133, 178);
+            this.chkPrecio.Name = "chkPrecio";
+            this.chkPrecio.Size = new System.Drawing.Size(214, 17);
+            this.chkPrecio.TabIndex = 9;
+            this.chkPrecio.Text = "Sólo Artículos con Precio Mayor a 1000";
+            this.chkPrecio.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 371);
+            this.Controls.Add(this.chkPrecio);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Demo de Carga de DataGridView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.CheckBox chkPrecio;
+    }
+}
+
---
+++ DemoCargaGrilla/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoCargaTextBoxMultilinea
+{
+    public partial class Form1 : Form
+    {
+
+        public struct ARTICULO
+        {
+            public string Nombre;
+            public int Codigo;
+            public float Precio;
+        }
+
+        // creamos el arreglo 
+        
+        const int MAX = 4; 
+        ARTICULO[] articulo;
+        int pos;
+
+        int control = 0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+            txtCodigo.Text = "";
+            txtNombre.Text = "";
+            txtPrecio.Text = ""; 
+
+            articulo= new ARTICULO[MAX];
+            pos = 0;
+
+            //limpiamos la grilla 
+            dgvDatos.Rows.Clear();  
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            int control = int.Parse(txtCodigo.Text);    
+            bool tieneRepetido = false;
+
+
+            for (int i = 0; i < pos; i++)
+            {
+                if (articulo[i].Codigo == control)
+                {
+                    tieneRepetido = true;
+                    MessageBox.Show("Numero repetido");
+                    txtCodigo.Text = "";
+                    break;
+                }
+            }
+
+
+            if (txtNombre.Text!= "" && txtCodigo.Text!= "" && txtPrecio.Text !="" )
+            {
+
+                 
+               //agregamos elementos 
+                articulo[pos].Nombre = txtNombre.Text;
+                articulo[pos].Codigo = int.Parse(txtCodigo.Text);
+                articulo[pos].Precio=float.Parse(txtPrecio.Text);
+                //iteramos para que agregue
+                
+                pos++;
+                //controlar si quedan espacios 
+                if (pos == MAX)
+                {
+                    MessageBox.Show("Datos llenos"); 
+                    btnAgregar.Enabled = false;
+                }
+                txtCodigo.Text = "";
+                txtNombre.Text = "";
+                txtPrecio.Text = "";
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            int i; 
+            if (chkPrecio.Checked)
+            {
+                 for ( i = 0; i < MAX; i++)
+                  {
+                      if (articulo[i].Precio > 1000) { 
+                           
+                           dgvDatos.Rows.Add(articulo[i].Codigo.ToString(),
+                                        articulo[i].Nombre,
+                                        articulo[i].Precio.ToString());
+
+                    }
+                    
+                }
+               
+            }
+            else
+            {
+                for (i = 0; i < pos; i++)
+                {
+                    // mostrar en la grilla
+                    dgvDatos.Rows.Add(articulo[i].Codigo.ToString(),
+                                        articulo[i].Nombre,
+                                        articulo[i].Precio.ToString());
+                }
+            }
+
+
+
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void txtCodigo_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void txtCodigo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!(e.KeyChar>=48 && e.KeyChar <= 57) && e.KeyChar != 8)
+            {
+                MessageBox.Show("Solo numeros"); 
+                e.Handled = true;
+                return; 
+            }
+        }
+    }
+}
